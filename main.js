48b24f9aa187b94f589840663de295d47e8aa64b
Arya Lad <95899975+aryalad0912@users.noreply.github.com>
2022-04-01 12:49:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var ComChoice = choices[i];
 var UserPoints = 0;
 var ComPoints = 0;
 function score(){
-	var score_div = document.getElementById("score").innerHTML = UserPoints + " - " + ComPoints;
+var score_div = document.getElementById("score").innerHTML = UserPoints + " - " + ComPoints;
 }
 setInterval(score, 50);
 function convert(word){
@@ -13,57 +13,45 @@ function convert(word){
 	return '<i class="far fa-hand-scissors"></i>'
 }
 function game(UserChoice){
-	var box = document.getElementById("challenge");
-	box.style.display = "inline-flex";
-	var userDiv = document.getElementById("YourObject");
-	userDiv.innerHTML = convert(UserChoice);
-	var comDiv = document.getElementById("ComObject");
-	comDiv.innerHTML = convert(ComChoice);
-	if(UserChoice === "paper" && ComChoice === "rock" || UserChoice === "rock" && ComChoice === "scissors" || UserChoice === "scissors" && ComChoice === "paper"){
-		win(UserChoice);
-	}
-	else if(UserChoice === ComChoice){
-		draw(UserChoice);
-	}
-	else{
-		lose(UserChoice);
-	}
-	function continuGame(){
-		i = Math.floor(Math.random() * 3);
-		ComChoice = choices[i];
-		box.style.display = "none";
-	}
-	setTimeout(continuGame, 1200);
+ var box = document.getElementById("challenge");
+ box.style.display = "inline-flex";
+ var userDiv = document.getElementById("YourObject");
+ userDiv.innerHTML = convert(UserChoice);
+ var comDiv = document.getElementById("ComObject");
+ comDiv.innerHTML = convert(ComChoice);
+ if(UserChoice === "paper" && ComChoice === "rock" || UserChoice === "rock" && ComChoice === "scissors" || UserChoice === "scissors" && ComChoice === "paper"){
+win(UserChoice);}
+else if(UserChoice === ComChoice){
+draw(UserChoice);
+}
+else{
+lose(UserChoice);}
+function continuGame(){
+i = Math.floor(Math.random() * 3);
+ ComChoice = choices[i];
+  box.style.display = "none";
+}
+ setTimeout(continuGame, 1200);
 }
 function win(bn){
-	UserPoints++;
-    document.getElementById("who").innerHTML = "You win!";
-    var bn = document.getElementById(bn);
-    bn.classList.remove("bn");
-    
-    setTimeout(() => {
-    	bn.classList.add("bn");
-        
-    }, 1200);
+UserPoints++;
+ document.getElementById("who").innerHTML = "You win!";
+var bn = document.getElementById(bn);
+bn.classList.remove("bn");
+ setTimeout(() => {
+    	bn.classList.add("bn");}, 1200);
 }
 function draw(bn){
-	document.getElementById("who").innerHTML = "It's a Draw.";
-	var bn = document.getElementById(bn);
-    bn.classList.remove("bn");
-    
-    setTimeout(() => {
-    	bn.classList.add("bn");
-        
-    }, 1200);
+document.getElementById("who").innerHTML = "It's a Draw.";
+var bn = document.getElementById(bn);bn.classList.remove("bn");
+ setTimeout(() => {
+    	bn.classList.add("bn");}, 1200);
 }
 function lose(bn){
-	ComPoints++;
-	document.getElementById("who").innerHTML = "You lose...";
-	var bn = document.getElementById(bn);
-    bn.classList.remove("bn");
-    
-    setTimeout(() => {
-    	bn.classList.add("bn");
-        
-    }, 1200);
+ComPoints++;
+document.getElementById("who").innerHTML = "You lose...";
+var bn = document.getElementById(bn);
+  bn.classList.remove("bn");
+ setTimeout(() => {
+ bn.classList.add("bn");}, 1200);
 }
